8a8c206ab5d372f37d1512c60dcbcae08a1c226d
Ro706 <rohitmandal99091@gmail.com>
2024-10-19 14:20:18 +0530

    update

--- JS Todo/index.js
+++ JS Todo/index.js
@@ -1,31 +1,122 @@
-// Select DOM elements
-const taskInput = document.getElementById('task');
-const addButton = document.getElementById('add');
-const taskList = document.getElementById('taskList');
+let todoRootEl = document.getElementById("todoRoot");
+let userInputEl = document.getElementById("userInput");
 
-// Initialize task array
-let tasks = ['Task 1', 'Task 2', 'Task 3'];
 
-// Function to render tasks
-function renderTasks() {
-    taskList.innerHTML = ''; // Clear the current list
-    tasks.forEach((task, index) => {
-        const li = document.createElement('li');
-        li.className = 'list-group-item';
-        li.textContent = task;
-        taskList.appendChild(li);
-    });
+
+let todoList = [
+
+    {
+        title: "HTML",
+        id: 1
+    },
+    {
+        title: "CSS",
+        id: 2
+    },
+    {
+        title: "Bootstrap",
+        id: 3
+    }
+
+]
+
+
+
+function onStatusChange(checkboxId, titleId) {
+
+    let checkBoxEl = document.getElementById(checkboxId);
+    let titleEl = document.getElementById(titleId);
+
+    if (checkBoxEl.checked == true) {
+
+        titleEl.classList.add("checked");
+
+    }
+    else {
+
+        titleEl.classList.remove("checked");
+
+    }
 }
 
-// Add event listener to the "Add" button
-addButton.addEventListener('click', () => {
-    const task = taskInput.value.trim();
-    if (task) {
-        tasks.push(task); // Add task to the array
-        taskInput.value = ''; // Clear the input field
-        renderTasks(); // Update the task list
-    }
-});
 
-// Initial render
-renderTasks();
+
+function onDeleteTodo(todoId){
+    let myTodo = document.getElementById(todoId);
+    todoRootEl.removeChild(myTodo);
+    let newId = parseInt(todoId.slice(4));
+    let index = todoList.findIndex((each)=> each.id === newId);
+    todoList.splice(index,1);
+}
+
+
+
+function createAndAppendTodo(todo) {
+
+    let checkboxId = "checkbox" + todo.id;
+    let titleId = "title" + todo.id;
+    let todoId = "todo" + todo.id;
+
+    let listCont = document.createElement("li");
+    listCont.classList.add("todo-list-cont");
+    listCont.id = todoId;
+    todoRootEl.appendChild(listCont);
+
+    let checkBoxEl = document.createElement("input");
+    checkBoxEl.type = "checkbox";
+    checkBoxEl.id = checkboxId;
+    checkBoxEl.onclick = function() {
+        onStatusChange(checkboxId, titleId);
+    }
+    listCont.appendChild(checkBoxEl);
+
+    console.log(checkBoxEl);
+
+    let labelEl = document.createElement("label");
+    labelEl.classList.add("label-cont");
+    labelEl.htmlFor = checkboxId;
+    listCont.appendChild(labelEl);
+
+    let titleEl = document.createElement("h4");
+    titleEl.textContent = todo.title;
+    titleEl.id = titleId;
+    labelEl.appendChild(titleEl);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.onclick = function() {
+        onDeleteTodo(todoId);
+    }
+    labelEl.appendChild(deleteBtn);
+
+    let trashIconEl = document.createElement("i");
+    deleteBtn.classList.add("fa-solid", "fa-trash");
+    deleteBtn.appendChild(trashIconEl);
+
+}
+
+
+
+for (each of todoList) {
+
+    createAndAppendTodo(each);
+
+}
+
+
+
+function onAddTodo() {
+
+    console.log(userInputEl.value);
+
+    let newTodo = {
+        title: userInputEl.value,
+        id: todoList.length + 1
+    }
+
+    createAndAppendTodo(newTodo);
+    todoList.push(newTodo);
+
+    console.log(todoList);
+
+}
